4e7ed1c62abc3d8db9eed08196d99d958dd1eb26
Dylan Davids <dylan@jhnc.nl>
2016-10-21 11:45:02 +0200

    Set user left message

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,10 @@ $('h1.room-title').text(room);
 
 socket.on('connect', function () {
 	console.log('Connected to socket.io server!');
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
 });
 
 socket.on('message', function(message) {
--- server.js
+++ server.js
@@ -7,8 +7,33 @@ var moment = require('moment');
 
 app.use(express.static(__dirname + '/public'));
 
+var clientInfo = {};
+
 io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
+
+	socket.on('disconnect', function () {
+		if (typeof clientInfo[socket.id] !== 'undefined') {
+			socket.leave(clientInfo[socket.id].room);
+
+			io.to(clientInfo[socket.id].room).emit('message', {
+				name: 'System',
+				text: clientInfo[socket.id].name + ' left the room',
+				timestamp: moment.valueOf()
+			});
+			delete clientInfo[socket.id];
+		}
+	});
+
+	socket.on('joinRoom', function(req){
+		clientInfo[socket.id] = req;
+		socket.join(req.room);
+		socket.broadcast.to(req.room).emit('message', {
+			name: 'System',
+			text: req.name +' joined the room',
+			timestamp: moment.valueOf()
+		});
+	});
 
 	socket.on('message', function (message) {
 		console.log('Message received: ' + message.text);
@@ -16,7 +41,7 @@ io.on('connection', function (socket) {
 		message.timestamp = moment().valueOf();
 		message.name = message.name;
 
-		io.emit('message', message);
+		io.to(clientInfo[socket.id].room).emit('message', message);
 	});
 
 	socket.emit('message', {
